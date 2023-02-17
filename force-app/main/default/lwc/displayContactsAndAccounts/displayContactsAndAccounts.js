02a415e8721f27d11d07796df6498de36606875d
Asmita singh <asmita.singh02@infosys.com>
2023-02-17 13:09:29 +0530

    commit

---
+++ force-app/main/default/lwc/displayContactsAndAccounts/displayContactsAndAccounts.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+import getAccountContactDataJs from '@salesforce/apex/GetAccountContactDataController.getAccountContactData';
+
+const columns = [
+    {label : 'Account Name', fieldName: 'accountName'},{label : 'Contact Name', fieldName: 'contactName'}];
+
+export default class DisplayContactsAndAccounts extends LightningElement {
+
+   columns = columns;
+   data = [];
+   isDataPresent = false;
+   connectedCallback(){
+    getAccountContactDataJs().then( data => {
+        console.log(' === '+ data);
+        if(data.length>0){
+            this.isDataPresent = true;
+            this.data = data;
+        }else{
+            this.false = true;
+        }
+    }).catch(error =>{
+        console.log('An error occurred ' + JSON.stringify(error));
+    });
+   }
+}
